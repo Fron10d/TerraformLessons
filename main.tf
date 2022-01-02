634e39e959be1d5bdc59bb3a3f503c73a0e12b7b
Cloud User <ec2-user@ip-172-31-46-219.us-east-2.compute.internal>
2022-01-02 06:52:02 +0000

    add data for ec2

--- main.tf
+++ main.tf
@@ -1,68 +1,56 @@
 provider "aws" {
+  region = "eu-central-1"
 }
 
 
-resource "aws_instance" "my_server_web" {
-  ami                    = "ami-98908709870987"
+resource "aws_instance" "my_webserver" {
+  ami                    = "ami-03a71cec707bfc3d7"
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.my_webserver.id]
+  user_data              = <<EOF
+#!/bin/bash
+yum -y update
+yum -y install httpd
+myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+echo "<h2>WebServer with IP: $myip</h2><br>Build by Terraform!"  >  /var/www/html/index.html
+sudo service httpd start
+chkconfig httpd on
+EOF
 
   tags = {
-    "Name" = "Server-Web"
-  }
-  // do not create until DB comes up
-  depends_on = [
-    aws_instance.my_server_db, aws_instance.my_server_app
-  ]
-}
-
-resource "aws_instance" "my_server_db" {
-  ami                    = "ami-98908709870987"
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.my_webserver.id]
-
-  tags = {
-    "Name" = "Server-DB"
+    Name = "Web Server Build by Terraform"
+    Owner = "Maks"
   }
 }
 
-resource "aws_instance" "my_server_app" {
-  ami                    = "ami-98908709870987"
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.my_webserver.id]
-
-  tags = {
-    "Name" = "Server-Application"
-  }
-  // do not create until DB comes up
-  depends_on = [
-    aws_instance.my_server_db
-  ]
-}
 
 resource "aws_security_group" "my_webserver" {
-  name = "My Security Group"
+  name = "WebServer Security Group"
+  description = "My First SecurityGroup"
 
-  dynamic "ingress" {
-    for_each = ["80", "443"]
-    content {
-      from_port   = ingress.value
-      to_port     = ingress.value
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    "Name" = "My Security Group"
+    Name = "Web Server SecurityGroup"
+    Owner = "Maks"
   }
-
 }
