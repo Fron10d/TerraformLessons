c1642a35fc7ff2825feb242d98c5435776f60ae6
Cloud User <ec2-user@ip-172-31-46-219.us-east-2.compute.internal>
2022-01-02 13:55:17 +0000

    Using vars

---
+++ Lesson18/variables.tf
@@ -0,0 +1,35 @@
+variable "region" {
+  description = "Please Enter AWS Region to deploy Server"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "instance_type" {
+  description = "Enter Instance Type"
+  type        = string
+  default     = "t3.small"
+}
+
+
+variable "allow_ports" {
+  description = "List of Ports to open for server"
+  type        = list
+  default     = ["80", "443", "22", "8080"]
+}
+
+variable "enable_detailed_monitoring" {
+  type    = bool
+  default = false
+}
+
+
+variable "common_tags" {
+  description = "Common Tags to apply to all resources"
+  type        = map
+  default = {
+    Owner       = "Maks"
+    Project     = "One"
+    CostCenter  = "777"
+    Environment = "development"
+  }
+}
