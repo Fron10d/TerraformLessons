fcbd9c3c87a541aaa2c28a230b033fe8f495d612
Cloud User <ec2-user@ip-172-31-46-219.us-east-2.compute.internal>
2022-01-03 05:39:07 +0000

    Using vars

---
+++ Lesson20/main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+
+data "aws_region" "current" {}
+data "aws_availability_zones" "available" {}
+
+locals {
+  full_project_name = "${var.environment}-${var.project_name}"
+  project_owner     = "${var.owner} owner of ${var.project_name}"
+}
+
+
+locals {
+  name = "Static IP"
+  country  = "Kz"
+  city     = "Almaty"
+  az_list  = join(",", data.aws_availability_zones.available.names)
+  region   = data.aws_region.current.description
+  location = "In ${local.region} there are AZ: ${local.az_list}"
+}
+
+resource "aws_eip" "my_static_ip" {
+  tags = {
+    Name       = local.name
+    Owner      = var.owner
+    Project    = local.full_project_name
+    proj_owner = local.project_owner
+    city       = local.city
+    region_azs = local.az_list
+    location   = local.location
+  }
+}
---
+++ Lesson20/variables.tf
@@ -0,0 +1,11 @@
+variable "environment" {
+  default = "DEV"
+}
+
+variable "project_name" {
+  default = "One"
+}
+
+variable "owner" {
+  default = "Maks"
+}
